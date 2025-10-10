55de4893db287266234ad0a5f35ec74f069adc54
Christophe Gaube <1050197+cgaube@users.noreply.github.com>
2025-10-09 20:07:44 -0700

    test being fancy

--- devcommand-test.rb
+++ devcommand-test.rb
@@ -1,18 +1,5 @@
-class DevcommandTest < Formula
-  desc "DevCommands modern multi-CLI applications"
-  homepage "https://github.com/cgaube/dev-commands"
-  head "https://github.com/cgaube/dev-commands.git", branch: "main"
+require_relative 'utils/bun_formula.rb'
 
-  depends_on "bun" => :build
-
-  def install
-    cd "packages/test" do
-      # install deps for just this package
-      system "bun", "install", "--no-save"
-      # compile it into a single binary
-      system "bun", "run", "compile"
-      # Install the bin/* in the devcommands homebrew etc folder
-      (prefix/"etc/devcommands").install "bin"
-    end
-  end
+class DevcommandTest < DevcommandBunFormula
+  install_package "test"
 end
---
+++ utils/bun_formula.rb
@@ -0,0 +1,19 @@
+class DevcommandBunFormula < Formula
+  homepage "https://github.com/cgaube/dev-commands"
+  head "https://github.com/cgaube/dev-commands.git", branch: "main"
+
+  depends_on "bun" => :build
+
+  def self.install_package(package_name)
+    define_method(:install) do
+      cd "packages/#{package_name}" do
+        # install deps for just this package
+        system "bun", "install", "--no-save"
+        # compile it into a single binary
+        system "bun", "run", "compile"
+        # Install the bin/* in the devcommands homebrew etc folder
+        (prefix/"etc/devcommands").install "bin/#{package_name}"
+      end
+    end
+  end
+end
