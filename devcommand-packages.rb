22d0d52477ba5112df0d1008dda69ef5b52cc11c
Christophe Gaube <1050197+cgaube@users.noreply.github.com>
2025-10-29 21:31:41 -0700

    fix path

--- devcommand-packages.rb
+++ devcommand-packages.rb
@@ -1,6 +1,6 @@
 require_relative "utils/bun_formula"
 
 class DevcommandPackages < DevcommandBunFormula
-  install_package "packages"
+  install_package "package"
   desc "Manage dev command packages"
 end
