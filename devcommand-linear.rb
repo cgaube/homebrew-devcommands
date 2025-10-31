e29e62a07673866609ddbff4ae19d93ca45e564c
Christophe Gaube <1050197+cgaube@users.noreply.github.com>
2025-10-30 20:34:01 -0700

    config improvement

--- devcommand-linear.rb
+++ devcommand-linear.rb
@@ -1,6 +1,6 @@
 require_relative "utils/bun_formula"
 
 class DevcommandLinear < DevcommandBunFormula
-  install_package "linear"
+  install_package "linear", has_config_files: true
   desc "Linear related helpers"
 end
--- utils/bun_formula.rb
+++ utils/bun_formula.rb
@@ -9,7 +9,7 @@ class DevcommandBunFormula < Formula
     end
   end
 
-  def self.install_package(package_name)
+  def self.install_package(package_name, has_config_files: false)
     define_method(:install) do
       cd "packages/#{package_name}" do
         # install deps for just this package
@@ -18,6 +18,23 @@ class DevcommandBunFormula < Formula
         system "bun", "run", "compile"
         # Install the bin/* in the devcommands homebrew etc folder
         (prefix/"etc/devcommands").install "bin/#{package_name}"
+      end
+    end
+
+    if has_config_files
+      define_method(:uninstall) do
+        # Remove the config folder explicitly
+        rm_rf etc/"devcommands-config/#{package_name}"
+      end
+
+      define_method(:caveats) do
+        <<~EOS
+          Configuration files are located at: #{etc}/devcommands-config/#{package_name}
+          These files will be removed automatically when you uninstall this formula.
+          -----
+          Note: Secret configurations are stored in the keychain and are not removed automatically.
+          To clear them, run: `dev #{package_name} config clear` before uninstalling this formula.
+        EOS
       end
     end
   end
