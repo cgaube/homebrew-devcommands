67487b25c309ab1f4b6e6c732ade11b8ff40a228
Christophe Gaube <1050197+cgaube@users.noreply.github.com>
2026-04-27 20:38:53 -0700

    run package (#1)

---
+++ devcommand-run.rb
@@ -0,0 +1,8 @@
+require_relative "utils/bun_formula"
+
+class DevcommandRun < DevcommandBunFormula
+  install_package "run"
+  desc "Run project scripts from any package manager"
+  # Version will automatically be set via github workflow action
+  version "2026.3.4-1772666664"
+end
